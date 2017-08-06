73761dc4dc1c920afff6c99b62e8ee6e11263dca
ivanzakh <ivanzakharov2011@gmail.com>
2017-08-06 14:38:40 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -43,12 +43,95 @@
     });
 
     // Scroll to first element
-    $('#lead-down span').click(function() {
+    $('#lead-down').on('click', 'span', function() {
         var scrollDistance = $('#lead').next().offset().top;
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
         }, 500);
     });
+
+  var header = $("header"); // Меню
+  var scrollPrev = 0; // Предыдущее значение скролла
+
+  $(window).on('scroll', function(){
+    var scrolled = $(window).scrollTop(); // Высота скролла в px
+    var firstScrollUp = false; // Параметр начала сколла вверх
+    var firstScrollDown = false; // Параметр начала сколла вниз
+
+    // Если скроллим
+    if ( scrolled > 0 ) {
+      // Если текущее значение скролла > предыдущего, т.е. скроллим вниз
+      if ( scrolled > scrollPrev ) {
+        firstScrollUp = false; // Обнуляем параметр начала скролла вверх
+        // Если меню видно
+        if ( scrolled < header.height() + header.offset().top ) {
+          // Если только начали скроллить вниз
+          if ( firstScrollDown === false ) {
+            var topPosition = header.offset().top; // Фиксируем текущую позицию меню
+            header.css({
+              "top": topPosition + "px"
+            });
+            firstScrollDown = true;
+          }
+          // Позиционируем меню абсолютно
+          header.css({
+            "position": "absolute"
+          });
+          // Если меню НЕ видно
+        } else {
+          // Позиционируем меню фиксированно вне экрана
+          header.css({
+            "position": "fixed",
+            "top": "-" + header.height() + "px"
+          });
+        }
+
+        // Если текущее значение скролла < предыдущего, т.е. скроллим вверх
+      } else {
+        firstScrollDown = false; // Обнуляем параметр начала скролла вниз
+        // Если меню не видно
+        if ( scrolled > header.offset().top ) {
+          // Если только начали скроллить вверх
+          if ( firstScrollUp === false ) {
+            var topPosition = header.offset().top; // Фиксируем текущую позицию меню
+            header.css({
+              "top": topPosition + "px"
+            });
+            firstScrollUp = true;
+          }
+          // Позиционируем меню абсолютно
+          header.css({
+            "position": "absolute"
+          });
+        } else {
+          // Убираем все стили
+          header.removeAttr("style");
+        }
+      }
+      // Присваеваем текущее значение скролла предыдущему
+      scrollPrev = scrolled;
+    }
+  });
+
+  /*function autoHideHeader() {
+    currentTop = $(window).scrollTop();
+
+    checkSimpleNavigation(currentTop);
+
+    previousTop = currentTop;
+    scrolling = false;
+  }
+
+  function checkSimpleNavigation(currentTop) {
+    //there's no secondary nav or secondary nav is below primary nav
+    if (previousTop - currentTop > scrollDelta) {
+      //if scrolling up...
+      mainHeader.removeClass('is-hidden');
+    } else if( currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
+      //if scrolling down...
+      mainHeader.addClass('is-hidden');
+    }
+  }*/
 
     // Create timeline
     $('#experience-timeline').each(function() {
