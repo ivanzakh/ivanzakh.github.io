66e85d04d376ed730ed25e0b709e3aa231c9f6a0
ivanzakh <ivanzakharov2011@gmail.com>
2019-04-21 21:57:03 +0300

    try to fix 2

--- js/scripts.js
+++ js/scripts.js
@@ -169,6 +169,11 @@
         });
     });
 
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.getElementById('lead').style.setProperty('--vh', `${vh}px`);  
+
   particlesJS('lead-overlay', {
     "particles": {
       "number": {
